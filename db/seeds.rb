3e4529e22d7e784b41518d78df9c1ebcb5560d6e
dannyknows <dannylow465@gmail.com>
2020-05-05 21:19:14 +1000

    half done seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,28 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user = User.new(
-    username: "admin",
-    email: "admin@admin.com",
-    password: "password123",
-    password_confirmation: "password123",
-    status: 69
+  username: "admin",
+  email: "admin@admin.com",
+  password: "password123",
+  password_confirmation: "password123",
+  status: 69
 )
 
 user.save!
 
 10.times do 
-    user = User.new(
-        username: Faker::Name.name,
-        email: Faker::Internet.email,
-        password: "password123",
-        password_confirmation: "password123"
-    )
-    user.save!
+  user = User.new(
+    username: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "password123",
+    password_confirmation: "password123"
+  )
+  user.save!
+end
+
+10.times do 
+  tag = Tag.create(
+    tag: Faker::Dessert.variety,
+  )
 end
 
