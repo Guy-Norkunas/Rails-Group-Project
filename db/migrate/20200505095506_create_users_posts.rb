c2148f8205b5750608d55d4122e68f7015ff8879
dannyknows <dannylow465@gmail.com>
2020-05-05 19:56:54 +1000

    fixed db relations

--- db/migrate/20200505095506_create_users_posts.rb
+++ db/migrate/20200505095506_create_users_posts.rb
@@ -1,8 +1,8 @@
 class CreateUsersPosts < ActiveRecord::Migration[6.0]
   def change
     create_table :users_posts do |t|
-      t.references :post_id, null: false, foreign_key: true
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_080844) do
+ActiveRecord::Schema.define(version: 2020_05_05_095506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,18 @@ ActiveRecord::Schema.define(version: 2020_05_05_080844) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "tag"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -48,5 +60,16 @@ ActiveRecord::Schema.define(version: 2020_05_05_080844) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "users_posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_users_posts_on_post_id"
+    t.index ["user_id"], name: "index_users_posts_on_user_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "users_posts", "posts"
+  add_foreign_key "users_posts", "users"
 end
