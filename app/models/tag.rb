1eedaa013fe43ae3d3931a35aa5cda03568e4922
Guy Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-05-05 07:41:39 -0700

    haha stupid rails thought you were smarter than me, you were wrong, me big brain know capital letters haha, it's not like I stayed up until past midnight or anything, baka

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
-    has_many :post_tags
-    has_many :posts, through: :post_tags
+    has_many :posts_tags
+    has_many :posts, through: :posts_tags
 end
--- db/migrate/20200505123617_create_post_tags.rb
+++
@@ -1,9 +0,0 @@
-class CreatePostTags < ActiveRecord::Migration[6.0]
-  def change
-    create_table :post_tags do |t|
-      t.references :tag, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
