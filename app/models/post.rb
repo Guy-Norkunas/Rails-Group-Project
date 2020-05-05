3e50e33cf0de91e3db21741336e56fdc8cd0ba93
Guy Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-05-05 16:52:21 -0700

    added one to many got userposts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to :image
   
   has_many :posts_tags
   has_many :tags, through: :posts_tags
+
+  has_many :users_posts
+  has_many :user, through: :users_posts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :images
+
+  has_many :users_posts
+  has_many :posts, through: :users_posts
 end
