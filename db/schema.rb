51aad3eba623ed07b3e8e70e6d8bdf5643bd72ac
Guy Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-05-05 03:51:16 -0700

    fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_100801) do
+ActiveRecord::Schema.define(version: 2020_05_05_105021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,14 +34,6 @@ ActiveRecord::Schema.define(version: 2020_05_05_100801) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "images", force: :cascade do |t|
-    t.string "location" #S3 Bucket URL
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_images_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -71,16 +63,15 @@ ActiveRecord::Schema.define(version: 2020_05_05_100801) do
   end
 
   create_table "users_posts", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
     t.index ["post_id"], name: "index_users_posts_on_post_id"
     t.index ["user_id"], name: "index_users_posts_on_user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "images", "users"
   add_foreign_key "users_posts", "posts"
   add_foreign_key "users_posts", "users"
 end
