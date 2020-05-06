187a8093deee306b3bf90e1cf88b5d13b97eaf69
Guy Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-05-05 22:44:38 -0700

    adding routes for profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root "post#index"
   resources :post
   devise_for :users
+
+  get "/profile/:id", to: "profile#show" as: "show_profile"
+  get "/profile", to: "profile#index", as: "user_profile"
+
 end
