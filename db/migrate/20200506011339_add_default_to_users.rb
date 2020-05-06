8baddf38a5c1010a8410096e7859fb4acadc16f2
Guy Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-05-05 18:21:04 -0700

    added validation to tags

---
+++ db/migrate/20200506011339_add_default_to_users.rb
@@ -0,0 +1,5 @@
+class AddDefaultToUsers < ActiveRecord::Migration[6.0]
+  def change
+    change_column_default :users, :status, 0
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,36 +8,41 @@ user = User.create(
 )
 
 10.times do 
-  user = User.new(
-    username: Faker::Name.name,
+  user = User.create(
+    username: Faker::JapaneseMedia::SwordArtOnline.game_name,
     email: Faker::Internet.email,
     password: "password123",
     password_confirmation: "password123"
   )
-  user.save!
 end
+
+sleep(0.5)
 p 'users created 🙋‍♀️'
 
 User.all.each do |user|
 	user.images.create(
 		url: "test"
 	)
-
 end
 
+sleep(0.5)
 p 'images created 🎆'
 
 User.all.each do |user|
 	i = 1
   user.posts.create(
-    content: "#{i + 1}" 
+    content: Faker::Movies::StarWars.quote
   )
 	i += 1
 	user.posts.create(
-    content: "#{i + 1}" 
+    content: Faker::JapaneseMedia::OnePiece.quote
 	)
 	i += 1
 end
+
+sleep(0.5)
+p 'posts created 🥴'
+
 Post.all.each do |post|
 	2.times do 
 		post.tags.create(
@@ -45,11 +50,10 @@ Post.all.each do |post|
 		)
 	end
 end
-p 'tags created 🏷'
-p "complete"
 
-# User.all.each do |user| 
-#   user.post.create(
-#     content: Faker::JapaneseMedia::OnePiece.quote
-#   )
-# end
+sleep(0.5)
+p 'tags created 🏷'
+
+
+sleep(0.5)
+p "complete"
