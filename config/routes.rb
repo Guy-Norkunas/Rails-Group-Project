2da79be3cab5588e7b07e37ead5dd8666b63063e
Guy Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-05-05 22:48:02 -0700

    fixed typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :post
   devise_for :users
 
-  get "/profile/:id", to: "profile#show" as: "show_profile"
+  get "/profile/:id", to: "profile#show", as: "show_profile"
   get "/profile", to: "profile#index", as: "user_profile"
 
 end
