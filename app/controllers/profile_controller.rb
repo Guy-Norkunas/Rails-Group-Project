99560bea3793b4b3e928bc2f1acb339d75e28519
Guy Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-05-06 00:12:14 -0700

    posts now display for user profiles and other profiles

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,9 @@
 class ProfileController < ApplicationController
+    before_action :authenticate_user!
     def index
-        p current_user
+        @posts = current_user.posts
+    end
+    def show
+        @posts = User.find(params[:id]).posts
     end
 end
