bdc165131213d7a6b0ece99818c716df891bd085
Guy Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-05-05 19:10:02 -0700

    unique username validation works, tests slightly broken

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
+class MyValidator < ActiveModel::Validator
+  def validate(user)
+     p "---------------"
+     p User.all
+     p "---------------"
+    if User.find_by_username(user.username)
+      user.errors[:username] << 'Username already exists'
+    end
+  end
+end
+
 class User < ApplicationRecord
+  include ActiveModel::Validations
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -11,4 +23,5 @@ class User < ApplicationRecord
   validates :status, presence: true
   validates :email, presence: true, length: {minimum: 4, maximum: 254}
   validates :status, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates_with MyValidator
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { described_class.new(
+  subject { described_class.create(
 		username: "guy",
 		email: "guy@gmail.com",
 		password: "password123",
@@ -26,6 +26,21 @@ RSpec.describe User, type: :model do
     it 'should not be created with short email' do
       subject.email = "a@a"
       expect(subject).to_not be_valid
+    end
+
+    it 'should not be created without status' do
+      subject.status = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should not exist if username is taken' do
+      temp_user = described_class.create(
+        username: "guy",
+        email: "random_email@gmail.com",
+        password: "password321",
+        password_confirmation: "password321"
+      )
+      expect(temp_user).to_not be_valid
     end
 	end
 end
