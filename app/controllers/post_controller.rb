d1f21a9d8ae67c3f9477bfe703543eef9c5fd31d
Guy Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-05-06 06:50:52 -0700

    added links for create/delete/show

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,5 +1,26 @@
 class PostController < ApplicationController
   before_action :authenticate_user!
   def index
+    @posts = Post.all
   end
+
+  def show
+    p Post.find(params[:id])
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    new_post = current_user.posts.new(post_params)
+    if new_post.save
+      redirect_to new_post
+    else
+      render 'index'
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:content)
+    end
+
 end
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,8 +1,10 @@
 class ProfileController < ApplicationController
     before_action :authenticate_user!
+    
     def index
         @posts = current_user.posts
     end
+
     def show
         @user = User.find(params[:id])
         @posts = @user.posts
